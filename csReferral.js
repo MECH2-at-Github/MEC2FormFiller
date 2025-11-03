57d8d67115388af900d99205ae3ad5dfb9f87d2e
MECH2-at-Github <MECH2.at.Github@gmail.com>
2025-11-03 11:13:43 -0600

    Update to using forEach, objects, etc.
    Converted childList from object to array in mec2functions using .map.

--- csReferral.js
+++ csReferral.js
@@ -15,30 +15,24 @@ async function csReferral(formInfo) {
 	const cpInfoFirst = form.getTextField('form1[0].P1[0].sfInfo[0].NameFirst[0]');
 	const ncpInfoLast = form.getTextField('form1[0].P1[0].sfOtherParent[0].NameLast[0]');
 	const ncpInfoFirst = form.getTextField('form1[0].P1[0].sfOtherParent[0].NameFirst[0]');
-	const childOneName = form.getTextField('form1[0].P1[0].sfChildren[0].Children[0].Row1[0].ChildName[0]');
-	const childTwoName = form.getTextField('form1[0].P1[0].sfChildren[0].Children[0].Row1[1].ChildName[0]');
-	const childThreeName = form.getTextField('form1[0].P1[0].sfChildren[0].Children[0].Row1[2].ChildName[0]');
-	const childFourName = form.getTextField('form1[0].P1[0].sfChildren[0].Children[0].Row1[3].ChildName[0]');
-	const childFiveName = form.getTextField('form1[0].P1[0].sfChildren[0].Children[0].Row1[4].ChildName[0]');
-	
+	const childrenObj = {
+		child0: form.getTextField('form1[0].P1[0].sfChildren[0].Children[0].Row1[0].ChildName[0]'),
+		child1: form.getTextField('form1[0].P1[0].sfChildren[0].Children[0].Row1[1].ChildName[0]'),
+		child2: form.getTextField('form1[0].P1[0].sfChildren[0].Children[0].Row1[2].ChildName[0]'),
+		child3: form.getTextField('form1[0].P1[0].sfChildren[0].Children[0].Row1[3].ChildName[0]'),
+		child4: form.getTextField('form1[0].P1[0].sfChildren[0].Children[0].Row1[4].ChildName[0]'),
+	}	
 	
 		// Info fields data
 	caseNumber.setText(formInfo.caseNumber);
-	form.getTextField('caseNumber1').setText(formInfo.caseNumber);
-	form.getTextField('caseNumber2').setText(formInfo.caseNumber);
-	form.getTextField('caseNumber3').setText(formInfo.caseNumber);
-	form.getTextField('caseNumber4').setText(formInfo.caseNumber);
-	workerName.setText(formInfo.workerName ?? "" + " - CCAP");
+	Array.from(['caseNumber1', 'caseNumber2', 'caseNumber3', 'caseNumber4', ]).forEach(field => { form.getTextField(field).setText(formInfo.caseNumber); })
+	workerName.setText(formInfo.workerName ? formInfo.workerName + " - CCAP" : "CCAP");
 	workerPhone.setText(formInfo.workerPhone ?? "");
 	cpInfoFirst.setText(formInfo.cpInfo.first);
 	cpInfoLast.setText(formInfo.cpInfo.last);
 	ncpInfoFirst.setText(formInfo.ncpInfo?.first);
 	ncpInfoLast.setText(formInfo.ncpInfo?.last);
-	childOneName.setText(formInfo.child0);
-	childTwoName.setText(formInfo.child1);
-	childThreeName.setText(formInfo.child2);
-	childFourName.setText(formInfo.child3);
-	childFiveName.setText(formInfo.child4);
+	formInfo.childList.forEach((childName, i) => { childrenObj["child" + i].setText(childName) })
 
 		//Name the PDF
 	const pdfEndName = ["Referral to Support and Collections", formInfo.caseNumber, formInfo.cpInfo.last, (formInfo.ncpInfo?.last + " " ?? "") + "(required)" ].join(" - ") + ".pdf"
