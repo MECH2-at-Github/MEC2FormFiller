f9b42f2b603cdbd232c90a60ba58f396092bb83d
MECH2-at-Github <MECH2.at.Github@gmail.com>
2025-12-08 13:43:04 -0600

    Moved index.js back into html.

--- jsFunctionLib.js
+++ jsFunctionLib.js
@@ -4,17 +4,17 @@ function strOrObjToHtmlDiv(info) {
 		let divContainer = createNewEle('div', { classList: "span-container" })
 		Object.entries(info).forEach( ([label, text] = []) => { divContainer.append(createNewEle('span', { textContent: label + ":", classList: "span-label" }), createNewEle('span', { classList: "span-text", textContent: text }) ) })
 		return divContainer
-	}
-}
-function utcDate(mdy) {
-	let [ utcM, utcD, utcY ] = mdy.split("/")
-	utcY = utcY.length === 2 ? utcY.padStart(4, "20") : utcY
-}
-
+	};
+};
 function createNewEle(nodeName, attribObj={}, dataObj) {
 	let newEle = Object.assign(document.createElement(nodeName), attribObj);
 	if (dataObj !== undefined) {
 		for (const [dataName, dataValue] of Object.entries(dataObj)) { newEle.dataset[dataName] = dataValue }
-	}
+	};
 	return newEle
+};
+function utcDate(mdy) {
+	let [ utcM, utcD, utcY ] = mdy.split("/")
+	utcY = utcY.length === 2 ? utcY.padStart(4, "20") : utcY
+	return Date.UTC( (utcY.length === 2 ? "20" + utcY : utcY), utcM-1, utcD )
 };
